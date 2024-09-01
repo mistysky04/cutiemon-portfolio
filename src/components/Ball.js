1f6507d96fd6e53e64052a81c4e3286998f14b1d
Hedie <hediemahmoudian@gmail.com>
2024-08-31 19:48:33 -0700

    added text for clicking on ball

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -3,7 +3,6 @@ import Text from './Text.js'
 import Spline from '@splinetool/react-spline';
 
 const Ball = () => {
-
     //Clicked hook
     const [clicked, setClicked] = useState(false);
 
@@ -11,33 +10,12 @@ const Ball = () => {
         setClicked(true);
     }
 
-    // if (clicked) {
-    //     return ( 
-    //         <>
-    //             <div className= "Ball" onClick={handleClick}>
-    //                 <Spline scene="https://prod.spline.design/0NaqQcDCZ766ZeK6/scene.splinecode" />
-    //                 <Text isBallClicked={clicked}></Text>
-    //             </div>
-    //         </>
-    //     )
-          
-    // } else {
-    //     return (
-    //     <>
-    //         <Spline scene="https://prod.spline.design/0NaqQcDCZ766ZeK6/scene.splinecode" />
-
-    //         {/* <div className= "Ball" onClick={handleClick}>
-    //             <img src={BallClose} width={200} height={200} alt="Closed cutieball, light pink with green leaves in center"></img>
-    //         </div> */}
-    //         <Text isBallClicked={clicked}></Text>
-    //     </>
-    //     )
-
     return ( 
-        <>
-            <Spline scene="https://prod.spline.design/0NaqQcDCZ766ZeK6/scene.splinecode" />
+        <>   
+            <div className= "BallAnimation" onMouseDown={handleClick}>
+                <Spline scene="https://prod.spline.design/0NaqQcDCZ766ZeK6/scene.splinecode"/>
+            </div>
             <Text isBallClicked={clicked}></Text>
-            
         </>
     )
 
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { TypeAnimation } from 'react-type-animation';
 
 
 
 const Text = ({ isBallClicked }) => {
-
+    // Need 2 diff keys bc they use the same classnames, otherwise won't rerender
     if (isBallClicked) {
-        return (<div className = "ClickedTextbox"></div>)
+        return (<div className = "TextBox" key="clicked">
+                    <div className= "TypingAnimation"> 
+                        <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'You got ...',
+                            1500, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Chubbles!',
+                            1500,
+                            'Lets visit a Cutiemon Gym trainer to train Chubbles'
+                        ]}
+                        wrapper="span"
+                        speed={30}
+                        style={{ fontSize: '3em', display: 'inline-block' }}
+                        repeat={0}
+                        omitDeletionAnimation= {true}
+                        cursor={false}/>
+                    </div>
+                </div>)
     } else {
-        return (<div className= "UnclickedTextBox">
+        return (<div className= "TextBox" key="notclicked">
                     <div className= "TypingAnimation"> 
                     <TypeAnimation
                     sequence={[
